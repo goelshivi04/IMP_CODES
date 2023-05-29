f5492e8a323b33fe1e4b5f49c44c50355bb15ffd
goelshivi04 <95372572+goelshivi04@users.noreply.github.com>
2023-05-29 15:17:14 +0530

    Update pair_sum.cpp

--- Hashing/pair_sum.cpp
+++ Hashing/pair_sum.cpp
@@ -17,3 +17,22 @@ public:
 
 //
 
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+        map<int,int>mp;
+        int c=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            int temp=k-arr[i];
+            if(mp.find(temp)!=mp.end())
+            {
+                c+=mp[temp];
+            }
+            mp[arr[i]]++;
+        }
+        return c;
+    }
+};
